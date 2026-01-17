31895b335709531c2ae38ecabc9e886c41745e68
panjyaswain <panjyaswain@gmail.com>
2026-01-17 16:19:30 +0530

    Assignment 1

---
+++ fact.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int greatest(int,int,int);
+void main()
+{
+	int x,y,z,a;
+	printf("enter x,y,z");
+	scanf("%d %d %d",&x,&y,&z);
+	a=greatest(x,y,z);
+	printf("%d is greater",a);
+	system("pause");
+}
+int greatest(int x,int y,int z)
+{
+	if(x>y)
+	{
+		if(x>z)
+			return x;
+		else
+		    return z;
+	}
+	else
+	{
+		if(y>z)
+			return y;
+		else
+			return z;
+	}
+		
+}
