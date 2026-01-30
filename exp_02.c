988bfb20bc970680b6893f111a1df972efb2a708
panjyaswain <panjyaswain@gmail.com>
2026-01-30 15:23:30 +0530

    ass.3

---
+++ exp_02.c
@@ -0,0 +1,19 @@
+# include<stdio.h>
+int main()
+{
+	int i;
+	char str[100];
+	printf("enter a string");
+	gets(str);
+	for(i=0; str[i]=100; i++)
+	printf("%c/n",str[i]);
+	return 0;
+}
+
+  
+	
+	
+
+	
+	
+
---
+++ exp_03.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <string.h>
+int main ()
+{
+char str[100];
+char reversestr[100];
+int length, i;
+printf("enter a string");
+gets(str);
+length = strlen(str);
+for (i=0; i<length/2; i++)
+{
+	char ch=str[length-1-i];
+	str[length-1-i]=str[i];
+	str[i]=ch;
+}
+printf("reverse string %s ",str);
+return 0;
+	
+	
+}
+
